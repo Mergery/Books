b65a9517975109c08baba760b9f6d7d8ef97810b
Mergery <magerywanjiru@gmail.com>
2022-06-30 18:40:45 +0300

    C#_Books

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+namespace Defining
+{
+    class Program
+    {
+        static void Main(string[]args)
+        {
+            Book b1 = new Book("War and Peace", "Leo", 825);
+            Book b2 = new Book("Heaven", "Calumn Scott", 123);
+            Book b3 = new Book("Maiden", "Orlando", 675);
+
+            Console.WriteLine(b1.GetDescription());
+            Console.WriteLine(b2.GetDescription());
+            Console.WriteLine(b3.GetDescription());
+        }
+
+    }
+    
+}
